2b3b544af081df5f1927c47f0722269707344d02
EstebanR3589 <estebanrodriguez3589@gmail.com>
2024-05-16 14:54:05 -0500

    creación código Tonny stark
    crear archivo programa y solución necesidad tony stark

---
+++ programa tony stark.cs
@@ -0,0 +1,14 @@
+/******************************************************************************
+NOMBRE: CESAR ESTEBAN BENITES RODRIGUEZ
+FECHA: 16-05-2024
+DESCRIPCION: 
+*******************************************************************************/
+using System;
+class HelloWorld
+{
+  static void Main(string[ ]args )
+  {
+    Console.Clear();
+    Console.WriteLine("Acceso denegado");
+  }
+}
